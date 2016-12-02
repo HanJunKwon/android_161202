55fb69bc9a8c563146d5a56e51b5b4fcab9304b6
권한준 <skclaqks11@naver.com>
2016-12-02 18:48:44 +0900

    레이아웃 완성

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.a403.myapplication;
 
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TimePicker;
+
 
 public class MainActivity extends AppCompatActivity {
+    CalendarView cv;
+    TimePicker tp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("201344080 권한준");
+        cv = (CalendarView)findViewById(R.id.calendarView);
+        tp = (TimePicker)findViewById(R.id.timePicker);
+
+        setButton();
     }
 }
