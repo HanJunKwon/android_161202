90041e6df1990be719394b6e43e1fa22b7efb27b
권한준 <skclaqks11@naver.com>
2016-12-02 18:57:18 +0900

    [예약시작] 버튼을 클릭하면 그 위에 chronometer가 작동

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.a403.myapplication;
 import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Chronometer;
 import android.widget.TimePicker;
 
 
 public class MainActivity extends AppCompatActivity {
+    Chronometer cm;
     CalendarView cv;
     TimePicker tp;
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,20 @@ public class MainActivity extends AppCompatActivity {
         setTitle("201344080 권한준");
         cv = (CalendarView)findViewById(R.id.calendarView);
         tp = (TimePicker)findViewById(R.id.timePicker);
+        cm = (Chronometer)findViewById(R.id.chronometer2);
+
 
         setButton();
+    }
+
+    void setButton(){
+        btn1 = (Button)findViewById(R.id.button);
+        btn2 = (Button)findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cm.start();
+            }
+        });
     }
 }
